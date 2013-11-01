eb46b940eb489b1476c1473b53c6af9b47d02688
dragos.neatu <dragos.neatu@DNEATU-PC.esp.local>
2013-11-01 13:01:49 +0200

    testing phase

--- dn-dba/src/main/java/ro/kdr/App.java
+++ dn-dba/src/main/java/ro/kdr/App.java
@@ -1,13 +1,36 @@
 package ro.kdr;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Tibi din nou!" );
-    }
+	 public static void main(String[] args) {  
+	      
+		  Configuration configuration=new Configuration();  
+		  configuration.configure();  
+		  SessionFactory sf=configuration.buildSessionFactory();  
+		    
+		  Session ss=sf.openSession();  
+		  ss.beginTransaction();  
+		 //saving objects to session  
+		    
+		  TestEntity user1=new TestEntity();  
+		  user1.setName("Arp2it");
+//		  user1.setId(1);
+		  ss.save(user1);  
+		  TestEntity user2=new TestEntity();  
+		  user2.setName("Ank3ita");
+//		  user2.setId(2);
+		  ss.save(user2);  
+		  ss.getTransaction().commit();  
+		  ss.close();  
+		    
+		 }  
+
 }
---
+++ dn-dba/src/main/java/ro/kdr/TestEntity.java
@@ -0,0 +1,29 @@
+package ro.kdr;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "TEST")
+public class TestEntity {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	@Column(name = "NAME")
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
